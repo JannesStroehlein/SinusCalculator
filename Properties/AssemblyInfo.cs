0a551d0265e4cfa60ea8c641534dd1afe4a5592f
Jannes Ströhlein <jati.stroehlein@gmail.com>
2021-04-30 15:12:58 +0200

    Assembly Info aktuallisiert

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("21.4.30.76")]
-[assembly: AssemblyFileVersion("21.4.30.76")]
+[assembly: AssemblyVersion("21.4.30.77")]
+[assembly: AssemblyFileVersion("21.4.30.77")]
 
-[assembly: AssemblyInformationalVersion("21.4.219")]
+[assembly: AssemblyInformationalVersion("21.4.220")]
